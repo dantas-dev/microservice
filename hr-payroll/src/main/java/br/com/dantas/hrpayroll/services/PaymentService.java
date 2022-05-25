60ccfc5b7433f05e8b9780f8b88aa60c7e1ba481
Rafael <developer.dantas@outlook.com>
2022-05-25 10:10:08 -0300

    Feign Client

--- hr-payroll/src/main/java/br/com/dantas/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/dantas/hrpayroll/services/PaymentService.java
@@ -1,29 +1,19 @@
 package br.com.dantas.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.dantas.hrpayroll.entities.Payment;
-import br.com.dantas.hrpayroll.entities.Worker;
+import br.com.dantas.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariable = new HashMap<>();
-		uriVariable.put("id", String.valueOf(workerId));
-		final var worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+		final var worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
